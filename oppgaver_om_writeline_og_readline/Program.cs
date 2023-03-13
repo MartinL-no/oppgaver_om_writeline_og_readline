e8f475fc10480f9a10c34ba0c1310228dcea7fc2
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-13 14:14:16 +0100

    oppgave-1

--- oppgaver_om_writeline_og_readline/Program.cs
+++ oppgaver_om_writeline_og_readline/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace oppgaver_om_writeline_og_readline
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hei, hva heter du?");
+
+      var Name = Console.ReadLine();
+
+      Console.WriteLine("Velkommen " + Name);
+
+    }
+  }
+}
